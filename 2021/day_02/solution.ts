d363b5c53a680e23b124c822f846c48f5c757fa8
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-02 17:12:34 +0100

    Add Day 2 input & solution

---
+++ 2021/day_02/solution.ts
@@ -0,0 +1,65 @@
+import assert from "assert";
+import R from "ramda";
+import { getInputLines } from "../getInputLines";
+
+// https://adventofcode.com/2021/day/2
+
+const commands = getInputLines("02").map((line) => {
+  const [direction, magnitude] = line.split(" ");
+
+  return { direction, magnitude: parseInt(magnitude, 10) };
+});
+
+// Part 1
+
+const solutionPart1 = R.reduce(
+  ({ position, depth }, { direction, magnitude }) => {
+    switch (direction) {
+      case "forward":
+        return { position: position + magnitude, depth };
+      case "up":
+        return { position, depth: depth - magnitude };
+      case "down":
+        return { position, depth: depth + magnitude };
+      default:
+        return { position, depth };
+    }
+  },
+  { position: 0, depth: 0 },
+  commands
+);
+
+const multipliedPart1 = solutionPart1.position * solutionPart1.depth;
+
+assert.equal(multipliedPart1, 1694130);
+
+// Part 2
+
+const solutionPart2 = R.reduce(
+  ({ position, depth, aim }, { direction, magnitude }) => {
+    switch (direction) {
+      case "forward":
+        return {
+          position: position + magnitude,
+          depth: depth + aim * magnitude,
+          aim,
+        };
+      case "up":
+        return { position, depth, aim: aim - magnitude };
+      case "down":
+        return { position, depth, aim: aim + magnitude };
+      default:
+        return { position, depth, aim };
+    }
+  },
+  { position: 0, depth: 0, aim: 0 },
+  commands
+);
+
+const multipliedPart2 = solutionPart2.position * solutionPart2.depth;
+
+assert.equal(multipliedPart2, 1698850445);
+
+//
+
+console.log({ solutionPart1, multipliedPart1, solutionPart2, multipliedPart2 });
