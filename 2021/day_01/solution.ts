8a1adc79662ac1d9f75b01a205e0b36ba61bfd3b
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-02 17:18:34 +0100

    Convert day 01 to ts

--- 2021/day_01/solution.ts
+++ 2021/day_01/solution.ts
@@ -1,6 +1,7 @@
-const R = require("ramda");
+import assert from "assert";
+import R from "ramda";
 
-const { getInputLines } = require("../getInputLines");
+import { getInputLines } from "../getInputLines";
 
 // https://adventofcode.com/2021/day/1
 
@@ -14,15 +15,22 @@ const solutionPart1 = R.pipe(
   R.length
 )(values);
 
+assert.equal(solutionPart1, 1759);
+
 // count the number of times the sum of measurements in this sliding window (3)
 // increases from the previous sum
 
 const solutionPart2 = R.pipe(
+  // @ts-expect-error
   R.aperture(3),
   R.map(R.sum),
   R.aperture(2),
   R.filter(([prev, current]) => prev < current),
   R.length
 )(values);
+
+assert.equal(solutionPart2, 1805);
+
+//
 
 console.log({ solutionPart1, solutionPart2 });
