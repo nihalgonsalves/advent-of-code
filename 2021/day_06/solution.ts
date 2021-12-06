56171d25dadf76671fb553a7eb11c3d2368e86b4
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-06 17:58:07 +0100

    Add Day 6 input & solution

---
+++ 2021/day_06/solution.ts
@@ -0,0 +1,36 @@
+import assert from "assert";
+import R from "ramda";
+
+import { getInputLines } from "../getInputLines";
+
+const [initialFish] = getInputLines("06");
+
+// <age, count>
+let fishState = R.countBy((age) => age, initialFish.split(","));
+
+const tick = () => {
+  fishState = {
+    "0": fishState["1"] ?? 0,
+    "1": fishState["2"] ?? 0,
+    "2": fishState["3"] ?? 0,
+    "3": fishState["4"] ?? 0,
+    "4": fishState["5"] ?? 0,
+    "5": fishState["6"] ?? 0,
+    // new fish
+    "6": (fishState["7"] ?? 0) + (fishState["0"] ?? 0),
+    "7": fishState["8"] ?? 0,
+    // reset fish
+    "8": fishState["0"] ?? 0,
+  };
+};
+
+R.times(tick, 80);
+const firstCount = R.sum(R.values(fishState));
+
+R.times(tick, 256 - 80);
+const secondCount = R.sum(R.values(fishState));
+
+assert.strictEqual(firstCount, 388739);
+assert.strictEqual(secondCount, 1741362314973);
+
+console.log({ firstCount, secondCount });
