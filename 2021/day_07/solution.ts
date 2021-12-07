eca9440743d58e69295055a6df1a49b9932c2b9c
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-07 10:46:38 +0100

    Add Day 7 solution part 2

--- 2021/day_07/solution.ts
+++ 2021/day_07/solution.ts
@@ -7,10 +7,41 @@ const [rawPositions] = getInputLines("07");
 
 const positions = rawPositions.split(",").map((s) => parseInt(s, 10));
 
+const costToMove = (dest: number, transform = (n: number) => n) =>
+  R.sum(positions.map((pos) => transform(Math.abs(pos - dest))));
+
+// Part 1
+
 const median = R.median(positions);
+const simpleCost = costToMove(median);
 
-const cost = R.sum(positions.map((pos) => Math.abs(pos - median)));
+// Part 2
 
-console.log({ cost });
+const mean = R.mean(positions);
 
-assert.strictEqual(cost, 345035);
+const calcIncreasingCost = (n: number) => R.sum(R.range(1, n + 1));
+
+const results = [Math.floor, Math.ceil].map((fn) => {
+  const value = fn(mean);
+  const cost = costToMove(value, calcIncreasingCost);
+
+  return { value, cost };
+});
+
+const [{ value: usedMeanValue, cost: increasingCost }] = R.sortBy(
+  (r) => r.cost,
+  results
+);
+
+// Solution
+
+assert.strictEqual(simpleCost, 345035);
+assert.strictEqual(increasingCost, 97038163);
+
+console.log({
+  median,
+  simpleCost,
+  mean,
+  usedMeanValue,
+  increasingCost,
+});
