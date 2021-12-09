ccbc8700fc281a90eed545dff9f66b5ebef7836c
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-09 12:10:21 +0100

    Add Day 9 input & solution

---
+++ 2021/day_09/solution.ts
@@ -0,0 +1,75 @@
+import assert from "assert";
+import R from "ramda";
+
+import { getInputLines } from "../getInputLines";
+
+type Point = { x: number; y: number; value: number };
+
+const data: Point[][] = getInputLines("09").map((line, rowIndex) =>
+  line.split("").map((s, colIndex) => ({
+    x: rowIndex,
+    y: colIndex,
+    value: parseInt(s, 10),
+  }))
+);
+
+const getNeighbours = (
+  x: number,
+  y: number,
+  predicate: (value: Point) => boolean = () => true
+) =>
+  [
+    data[x]?.[y - 1],
+    data[x]?.[y + 1],
+    data[x - 1]?.[y],
+    data[x + 1]?.[y],
+  ].filter((val): val is Point => val !== undefined && predicate(val));
+
+const getBasin = (point: Point, visited: boolean[][]): Point[] => {
+  const basinNeighbours = getNeighbours(
+    point.x,
+    point.y,
+    (p) => !visited[p.x][p.y] && p.value < 9
+  );
+
+  basinNeighbours.forEach(({ x, y }) => {
+    visited[x][y] = true;
+  });
+
+  return [point, ...basinNeighbours.flatMap((p) => getBasin(p, visited))];
+};
+
+let riskScore = 0;
+const basinSizes: number[] = [];
+
+data.forEach((row) => {
+  row.forEach((cell) => {
+    const neighbours = getNeighbours(cell.x, cell.y);
+
+    if (neighbours.every(({ value }) => value > cell.value)) {
+      riskScore += cell.value + 1;
+
+      const visited = Array.from({ length: data.length }, () =>
+        Array.from({ length: row.length }, () => false)
+      );
+
+      visited[cell.x][cell.y] = true;
+
+      basinSizes.push(getBasin(cell, visited).length);
+    }
+  });
+});
+
+const biggestBasinsMultipledSize = R.pipe(
+  () => basinSizes,
+  R.sort((a, b) => b - a),
+  R.take(3),
+  R.reduce<number, number>(R.multiply, 1)
+)();
+
+// Solution
+
+assert.strictEqual(riskScore, 591);
+assert.strictEqual(biggestBasinsMultipledSize, 1113424);
+
+console.log({ riskScore, biggestBasinsMultipledSize });
