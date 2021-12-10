ec9db9cf297a865aea698bdfe87faa5e9c465b5a
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-12-10 10:15:10 +0100

    Add Day 10 solution part 2

--- 2021/day_10/solution.ts
+++ 2021/day_10/solution.ts
@@ -5,41 +5,63 @@ import { getInputLines } from "../getInputLines";
 
 const lines = getInputLines("10").map((line) => line.split(""));
 
-const symbols: Record<string, string> = {
-  ")": "(",
-  "]": "[",
-  "}": "{",
-  ">": "<",
+const openingSymbols = ["(", "[", "{", "<"];
+const closingSymbols = [")", "]", "}", ">"];
+
+const openingToClosing = R.zipObj(openingSymbols, closingSymbols);
+const closingToOpening = R.zipObj(closingSymbols, openingSymbols);
+
+const syntaxScores: Record<string, { error: number; completion: number }> = {
+  ")": { error: 3, completion: 1 },
+  "]": { error: 57, completion: 2 },
+  "}": { error: 1197, completion: 3 },
+  ">": { error: 25137, completion: 4 },
 };
 
-const closingSymbols = Object.keys(symbols);
-const openingSymbols = Object.values(symbols);
+type ResolvedLine = { type: "corrupted" | "completed"; score: number };
 
-const syntaxErrorScore: Record<string, number> = {
-  ")": 3,
-  "]": 57,
-  "}": 1197,
-  ">": 25137,
-};
+const resolvedLines: ResolvedLine[] = lines.map((line) => {
+  const stack: string[] = [];
 
-const errorScore = R.sum(
-  lines.map((line) => {
-    const stack: string[] = [];
-
-    for (const sym of line) {
-      if (openingSymbols.includes(sym)) {
-        stack.push(sym);
-      } else if (closingSymbols.includes(sym) && stack.pop() !== symbols[sym]) {
-        return syntaxErrorScore[sym];
-      }
+  for (const sym of line) {
+    if (openingSymbols.includes(sym)) {
+      stack.push(sym);
+    } else if (
+      closingSymbols.includes(sym) &&
+      stack.pop() !== closingToOpening[sym]
+    ) {
+      return { type: "corrupted", score: syntaxScores[sym].error };
     }
+  }
 
-    return 0;
-  })
-);
+  const completionString = R.reverse(stack).map((s) => openingToClosing[s]);
+  const completionScore = completionString.reduce(
+    (score, s) => score * 5 + syntaxScores[s].completion,
+    0
+  );
+
+  return { type: "completed", score: completionScore };
+});
+
+const [completedScores, corruptedScores] = R.pipe(
+  R.partition<ResolvedLine>(({ type }) => type === "completed"),
+  R.map(R.map(({ score }) => score))
+)(resolvedLines);
+
+// Part 1
+
+const errorScore = R.sum(corruptedScores);
+
+// Part 2
+
+const completionScores = R.sort((a, b) => a - b, completedScores);
+
+const completionScore =
+  completionScores[Math.floor(completionScores.length / 2)];
 
 // Solution
 
 assert.strictEqual(errorScore, 341823);
+assert.strictEqual(completionScore, 2801302861);
 
-console.log(errorScore);
+console.log({ errorScore, completionScore });
