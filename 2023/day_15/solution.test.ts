31b87f52c26e12fa0e4d2bce039a46801af78288
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-12-15 22:41:35 +0100

    Add 2023/15 part 1

---
+++ 2023/day_15/solution.test.ts
@@ -0,0 +1,36 @@
+import { describe, it, expect } from "bun:test";
+
+import { run1, run2 } from "./solution";
+import { getInputLines } from "../../getInputLines";
+
+const input = await getInputLines(import.meta.url);
+
+const sample: string[] = [
+  "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7",
+];
+
+describe("day 15", () => {
+  describe("part 1", () => {
+    it("should return the correct value for HASH", () => {
+      expect(run1(["HASH"])).toBe(52);
+    });
+
+    it("should return the correct sample value", () => {
+      expect(run1(sample)).toBe(1320);
+    });
+
+    it("should return the correct value", () => {
+      expect(run1(input)).toBe(511343);
+    });
+  });
+
+  describe("part 2", () => {
+    it("should return the correct sample value", () => {
+      expect(run2(sample)).toBe(0);
+    });
+
+    it("should return the correct value", () => {
+      expect(run2(input)).toBe(0);
+    });
+  });
+});
---
+++ 2023/day_15/solution.ts
@@ -0,0 +1,20 @@
+import * as R from "ramda";
+
+export const run1 = (input: string[]): number => {
+  const items = input[0].split(",");
+
+  const sums = items.map((item) =>
+    item.split("").reduce((acc, curr) => {
+      let val = acc + curr.charCodeAt(0);
+      val *= 17;
+      val %= 256;
+      return val;
+    }, 0),
+  );
+
+  return R.sum(sums);
+};
+
+export const run2 = (input: string[]): number => {
+  return 0;
+};
