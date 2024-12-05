b9b900dfa1657d879791f645e7742fd7634c9692
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-12-05 17:59:57 +0100

    feat: add 2024/05 part 1

---
+++ 2024/day_05/solution.test.ts
@@ -0,0 +1,59 @@
+import { describe, expect, it } from "bun:test";
+
+import { getInput } from "../../getInputLines";
+import { run1, run2 } from "./solution";
+
+const input = await getInput(import.meta.url);
+
+const sample = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+
+`;
+
+describe("day 05", () => {
+	describe("part 1", () => {
+		it("should return the correct sample value", () => {
+			expect(run1(sample)).toBe(143);
+		});
+
+		it("should return the correct value", () => {
+			expect(run1(input)).toBe(5639);
+		});
+	});
+
+	describe("part 2", () => {
+		it("should return the correct sample value", () => {
+			expect(run2(sample)).toBe(0);
+		});
+
+		it("should return the correct value", () => {
+			expect(run2(input)).toBe(0);
+		});
+	});
+});
---
+++ 2024/day_05/solution.ts
@@ -0,0 +1,45 @@
+const parseInput = (input: string) => {
+	const [pageOrderingRulesStr, pagesStr] = input.trim().split("\n\n");
+
+	const pageOrderingRules = pageOrderingRulesStr.split("\n").map((line) => {
+		const [left, right] = line
+			.split("|")
+			.map((str) => Number.parseInt(str, 10));
+
+		return [left, right] as const;
+	});
+
+	const pages = pagesStr
+		.split("\n")
+		.map((line) => line.split(",").map((str) => Number.parseInt(str, 10)));
+
+	const pageOrdering: Record<number, number[]> = {};
+	for (const [left, right] of pageOrderingRules) {
+		pageOrdering[left] ??= [];
+		pageOrdering[left].push(right);
+	}
+
+	return { pageOrdering, pages };
+};
+
+export const run1 = (input: string): number => {
+	const { pageOrdering, pages } = parseInput(input);
+
+	return pages
+		.filter((page) => {
+			const pageIndex = Object.fromEntries(
+				page.map((page, index) => [page, index]),
+			);
+
+			return page.every((update, index) =>
+				(pageOrdering[update] ?? []).every(
+					(right) => pageIndex[right] == null || index < pageIndex[right],
+				),
+			);
+		})
+		.reduce((acc, page) => acc + page[(page.length - 1) / 2], 0);
+};
+
+export const run2 = (input: string): number => {
+	return 0;
+};
