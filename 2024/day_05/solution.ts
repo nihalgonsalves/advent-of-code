b1ac07a9837133666d900a7a6b314d32c665f711
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-12-05 18:22:25 +0100

    refactor: use sort for both parts of 2024/05

--- 2024/day_05/solution.ts
+++ 2024/day_05/solution.ts
@@ -1,3 +1,5 @@
+import { isDeepStrictEqual } from "node:util";
+
 const parseInput = (input: string) => {
 	const [pageOrderingRulesStr, pagesStr] = input.trim().split("\n\n");
 
@@ -23,52 +25,38 @@ const parseInput = (input: string) => {
 	return { pageOrdering, pages };
 };
 
+const sortPages = (
+	pages: number[][],
+	pageOrdering: Record<number, Set<number>>,
+) =>
+	pages.map((page) =>
+		page.toSorted((a, b) => {
+			if (pageOrdering[a]?.has(b)) {
+				return -1;
+			}
+
+			if (pageOrdering[b]?.has(a)) {
+				return 1;
+			}
+
+			return 0;
+		}),
+	);
+
 export const run1 = (input: string): number => {
 	const { pageOrdering, pages } = parseInput(input);
+	const sortedPages = sortPages(pages, pageOrdering);
 
-	return pages
-		.filter((page) => {
-			const pageIndex = Object.fromEntries(
-				page.map((page, index) => [page, index]),
-			);
-
-			return page.every((update, index) =>
-				[...(pageOrdering[update]?.values() ?? [])].every(
-					(right) => pageIndex[right] == null || index < pageIndex[right],
-				),
-			);
-		})
+	return sortedPages
+		.filter((sortedPage, i) => isDeepStrictEqual(pages[i], sortedPage))
 		.reduce((acc, page) => acc + page[(page.length - 1) / 2], 0);
 };
 
 export const run2 = (input: string): number => {
 	const { pageOrdering, pages } = parseInput(input);
+	const sortedPages = sortPages(pages, pageOrdering);
 
-	const unorderedPages = pages.filter((page) => {
-		const pageIndex = Object.fromEntries(
-			page.map((page, index) => [page, index]),
-		);
-
-		return page.some((update, index) =>
-			[...(pageOrdering[update]?.values() ?? [])].some(
-				(right) => pageIndex[right] !== null && index > pageIndex[right],
-			),
-		);
-	});
-
-	return unorderedPages
-		.map((page) =>
-			page.sort((a, b) => {
-				if (pageOrdering[a]?.has(b)) {
-					return -1;
-				}
-
-				if (pageOrdering[b]?.has(a)) {
-					return 1;
-				}
-
-				return 0;
-			}),
-		)
+	return sortedPages
+		.filter((sortedPage, i) => !isDeepStrictEqual(pages[i], sortedPage))
 		.reduce((acc, page) => acc + page[(page.length - 1) / 2], 0);
 };
