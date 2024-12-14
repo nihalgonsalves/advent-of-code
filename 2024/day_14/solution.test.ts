1781b7011a2f0df97e904c8298beb3761ee0fdc0
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-12-14 11:33:08 +0530

    feat: add 2024/14 part 1

---
+++ 2024/day_14/solution.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, it } from "bun:test";
+
+import { getInputLines } from "../../getInputLines";
+import { run1, run2 } from "./solution";
+
+const input = await getInputLines(import.meta.url);
+
+const sample: string[] = [
+	"p=0,4 v=3,-3",
+	"p=6,3 v=-1,-3",
+	"p=10,3 v=-1,2",
+	"p=2,0 v=2,-1",
+	"p=0,0 v=1,3",
+	"p=3,0 v=-2,-2",
+	"p=7,6 v=-1,-3",
+	"p=3,0 v=-1,-2",
+	"p=9,3 v=2,3",
+	"p=7,3 v=-1,2",
+	"p=2,4 v=2,-3",
+	"p=9,5 v=-3,-3",
+];
+
+describe("day 14", () => {
+	describe("part 1", () => {
+		it("should return the correct sample value", () => {
+			expect(run1(sample, 11, 7)).toBe(12);
+		});
+
+		it("should return the correct value", () => {
+			expect(run1(input, 101, 103)).toBe(233709840);
+		});
+	});
+
+	describe("part 2", () => {
+		it("should return the correct sample value", () => {
+			expect(run2(sample)).toBe(0);
+		});
+
+		it("should return the correct value", () => {
+			expect(run2(input)).toBe(0);
+		});
+	});
+});
---
+++ 2024/day_14/solution.ts
@@ -0,0 +1,108 @@
+type Robot = {
+	p: { x: number; y: number };
+	v: { x: number; y: number };
+};
+
+const parseInput = (input: string[]): Robot[] => {
+	return input.map((input) => {
+		const matches = /p=(?<px>\d+),(?<py>\d+) v=(?<vx>-?\d+),(?<vy>-?\d+)/.exec(
+			input,
+		);
+
+		if (!matches?.groups) {
+			throw new Error("Invalid input");
+		}
+
+		return {
+			p: {
+				x: Number.parseInt(matches.groups.px, 10),
+				y: Number.parseInt(matches.groups.py, 10),
+			},
+			v: {
+				x: Number.parseInt(matches.groups.vx, 10),
+				y: Number.parseInt(matches.groups.vy, 10),
+			},
+		};
+	});
+};
+
+const getQuadrants = (width: number, height: number) => ({
+	topLeft: {
+		x: [0, (width - 1) / 2 - 1],
+		y: [0, (height - 1) / 2 - 1],
+	},
+	bottomLeft: {
+		x: [0, (width - 1) / 2 - 1],
+		y: [(height - 1) / 2 + 1, height - 1],
+	},
+	topRight: {
+		x: [(width - 1) / 2 + 1, width - 1],
+		y: [0, (height - 1) / 2 - 1],
+	},
+	bottomRight: {
+		x: [(width - 1) / 2 + 1, width - 1],
+		y: [(height - 1) / 2 + 1, height - 1],
+	},
+});
+
+const moveRobotsOnce = (robots: Robot[], width: number, height: number) => {
+	for (const robot of robots) {
+		robot.p.x += robot.v.x;
+		robot.p.y += robot.v.y;
+
+		if (robot.p.x > width - 1) {
+			robot.p.x -= width;
+		} else if (robot.p.x < 0) {
+			robot.p.x += width;
+		}
+
+		if (robot.p.y > height - 1) {
+			robot.p.y -= height;
+		} else if (robot.p.y < 0) {
+			robot.p.y += height;
+		}
+	}
+};
+
+const getCountMap = (robots: Robot[]) => {
+	const map = new Map<string, number>();
+	for (const robot of robots) {
+		map.set(
+			`${robot.p.x},${robot.p.y}`,
+			(map.get(`${robot.p.x},${robot.p.y}`) ?? 0) + 1,
+		);
+	}
+	return map;
+};
+
+export const run1 = (
+	input: string[],
+	width: number,
+	height: number,
+): number => {
+	const robots = parseInput(input);
+
+	for (const _ of Array.from({ length: 100 })) {
+		moveRobotsOnce(robots, width, height);
+	}
+
+	const countMap = getCountMap(robots);
+
+	return Object.values(getQuadrants(width, height))
+		.map((quadrant) => {
+			let count = 0;
+
+			for (let iX = quadrant.x[0]; iX <= quadrant.x[1]; iX++) {
+				for (let iY = quadrant.y[0]; iY <= quadrant.y[1]; iY++) {
+					count += countMap.get(`${iX},${iY}`) ?? 0;
+				}
+			}
+
+			return count;
+		})
+		.reduce((acc, count) => acc * count, 1);
+};
+
+export const run2 = (input: string[]): number => {
+	return 0;
+};
